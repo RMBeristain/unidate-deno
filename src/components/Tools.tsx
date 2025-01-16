7d4915c7f59c4dbb28357e4ff7e011a11343d143
RMBeristain <virtualraider+github@gmail.com>
2025-01-17 09:35:06 +1100

    Makes label darker

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -7,7 +7,7 @@ const Tools = () => {
     return (
         <section className="bg-sky-50 dark:bg-slate-600 px-4 py-10">
             <div className="container-xl lg:container m-auto">
-                <h2 className="doto-rounded text-3xl font-bold text-sky-500 dark:text-sky-200 mb-6 text-center">
+                <h2 className="doto-rounded text-3xl font-bold text-sky-700 dark:text-sky-200 mb-6 text-center">
                     Convert Dates
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">

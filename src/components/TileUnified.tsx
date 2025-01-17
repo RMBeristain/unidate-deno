32794c161aeb17ade7e89db806de737d1732ce97
RMBeristain <virtualraider+github@gmail.com>
2025-01-17 15:32:13 +1100

    Update unified date on change

--- src/components/TileUnified.tsx
+++ src/components/TileUnified.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
-import {
-    getGregorianDate,
-    Style,
-    UnifiedDate,
-    Variant,
-} from "../UniDateConverter/Index";
+import { Style, UnifiedDate, Variant } from "../UniDateConverter/Index";
 
 const TileUnified = () => {
-    const [userDate, setUserDate] = useState<UnifiedDate>(
-        new UnifiedDate(getGregorianDate("iso")),
-    );
+    let userUnidate = new UnifiedDate(); // Today's date by default.
+    const [userDate, setUserDate] = useState<string>("");
+    const [uniDate, setUniDate] = useState<UnifiedDate>(userUnidate);
     const handleSubmit = (event: React.FormEvent) => event.preventDefault();
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let newUserDate = event.target.value;
+        setUserDate(newUserDate);
+        let isValidDate = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/.test(newUserDate);
+        if (isValidDate) {
+            userUnidate.reverse_unidate(newUserDate);
+            setUniDate(userUnidate);
+        }
+    };
 
     return (
         <form onSubmit={handleSubmit}>
@@ -19,27 +23,15 @@ const TileUnified = () => {
                     Unified
                 </div>
                 <h3 className="text-xl font-extrabold text-sky-500 dark:text-sky-500">
-                    {userDate.format_date(Variant.UNI, Style.LONG)}
+                    {uniDate.format_date(Variant.UNI, Style.LONG)}
                 </h3>
             </div>
 
             <input
                 id="UnifiedDate"
-                defaultValue={new UnifiedDate(getGregorianDate("iso"))
-                    .format_date(
-                        Variant.UNI,
-                        Style.ISO,
-                    )}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    const date = event.target.value;
-                    let newDate = new UnifiedDate();
-                    if (date) {
-                        newDate.reverse_unidate(date);
-                    } else {
-                        newDate.unify();
-                    }
-                    setUserDate(newDate);
-                }}
+                value={userDate}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    handleChange(event)}
                 className="mt-1 focus:ring-rose-500 focus:border-rose-500 block border-2
                         shadow-md sm:text-sm text-center border-sky-100 dark:border-sky-500 dark:bg-slate-400
                         dark:text-black rounded-md mb-8"
@@ -52,13 +44,13 @@ const TileUnified = () => {
                 <div className="flex flex-col justify-between mb-1">
                     <div className="tileTitle">Austral:</div>
                     <div className="text-lg">
-                        {userDate.format_date(Variant.AUS, Style.LONG)}
+                        {uniDate.format_date(Variant.AUS, Style.LONG)}
                     </div>
                 </div>
                 <div className="flex flex-col justify-between mb-1">
                     <div className="tileTitle">Territorian:</div>
                     <div className="text-lg">
-                        {userDate.format_date(Variant.SWT, Style.LONG)}
+                        {uniDate.format_date(Variant.SWT, Style.LONG)}
                     </div>
                 </div>
             </h3>
@@ -72,12 +64,18 @@ const TileUnified = () => {
                     </i>
                     YYYY-QM-DD
                 </div>
-                <a
-                    href=""
+                <button
+                    type="submit"
                     className="h-[36px] appButton text-center text-sm"
+                    onClick={() => {
+                        setUserDate(
+                            userUnidate.format_date(Variant.UNI, Style.ISO),
+                        );
+                        setUniDate(userUnidate);
+                    }}
                 >
                     Reset date
-                </a>
+                </button>
             </div>
         </form>
     );
